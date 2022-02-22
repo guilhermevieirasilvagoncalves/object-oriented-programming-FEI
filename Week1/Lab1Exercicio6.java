f0ce9480a61f6e02dfe8f647d913ec2c134dd06b
Guilherme Vieira <unifggoncalves@fei.edu.br>
2022-02-21 21:03:48 -0300

    week1

--- Week1/Lab1Exercicio6.java
+++ Week1/Lab1Exercicio6.java
@@ -7,11 +7,13 @@
 public class Lab1Exercicio6 {
     public static void main(String args[]){
         int catetoA, catetoB, hipotenusaC;
-        for(hipotenusaC = 0; hipotenusaC <= 500; hipotenusaC++){
-            for(catetoA = 0 ; catetoA <= 500; catetoA++){
-                for(catetoB = 0; catetoB <= 500; catetoB++){
+        int count = 0;
+        for(hipotenusaC = 1; hipotenusaC < 500; hipotenusaC++){
+            for(catetoA = 1 ; catetoA < 500; catetoA++){
+                for(catetoB = 1; catetoB < 500; catetoB++){
                     if(Math.pow(hipotenusaC,2) == (Math.pow(catetoA,2) + Math.pow(catetoB,2))){
                         System.out.printf("São números de pitágoras: %d %d %d\n",hipotenusaC,catetoA,catetoB);
+                        //count++;
                     }
                     else if(Math.pow(hipotenusaC,2) < (Math.pow(catetoA,2) + Math.pow(catetoB,2))){
                         assert true;
@@ -19,5 +21,6 @@ public class Lab1Exercicio6 {
                 }
             }
         }
+        //System.out.println(count);
     }
 }
